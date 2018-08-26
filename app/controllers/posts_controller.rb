69e7d91e2d36e6a7fd43e17ac460ee884dde8aea
Nancy Jo <enciie@gmail.com>
2018-08-26 10:48:47 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	def update
   	@post = Post.find(params[:id])
-		if @post.update_attribute(params[:post])
+		if @post.update_attributes(params[:post])
 			redirect_to post_path(@post)
 		end
 	end
